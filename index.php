e18bc7a687c02c7ccf11b2a2dc80659beb2fb364
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-09-06 16:48:46 +0300

    wip

--- index.php
+++ index.php
@@ -239,10 +239,10 @@
 		foreach ($ouritems as $key => $item) {
 			//grab the sales prior to the start of this week
 			$tempstats = grab_data_from_url($mainurl.'items?itemid='.$item['id'].'&date='.strtotime('-1 day',strtotime($this_week_range[0])));
-			var_dump($tempstats);
+			
 			//substract them from the sales to the day
-			$thisweek[$key] = (int)grab_data_from_url($mainurl.'items?itemid='.$item['id']);
-			$thisweek[$key] -= (int)$tempstats;
+			$thisweek[$key] = grab_data_from_url($mainurl.'items?itemid='.$item['id'])['sales'];
+			$thisweek[$key] -= $tempstats['sales'];
 		}
 	}
 	
@@ -252,8 +252,8 @@
 		$tempstats = grab_data_from_url($mainurl.'items?itemid='.$item['id'].'&date='.strtotime('-1 day',strtotime($last_week_range[0])));
 
 		//substract them from the sales to the day
-		$lastweek[$key] = (int)grab_data_from_url($mainurl.'items?itemid='.$item['id'].'&date='.strtotime($last_week_range[1]));
-		$lastweek[$key] -= (int)$tempstats;
+		$lastweek[$key] = grab_data_from_url($mainurl.'items?itemid='.$item['id'].'&date='.strtotime($last_week_range[1]))['sales'];
+		$lastweek[$key] -= $tempstats['sales'];
 	}
 	var_dump($thisweek, $lastweek);
 	//stats for tags
